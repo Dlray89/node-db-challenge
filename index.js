63c6a04ba47947bb5adb2005e024f7ce5ef3ee03
dlray89 <dlrayjr89@gmail.com>
2020-03-20 11:10:51 -0400

    basic server set up and it was connected successfully

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 const server = require("./server")
 
 const PORT = process.env.PORT || 6000
 
 server.listen(PORT, () => {
-    console.log(`\n *** server running on http://localhost:5000 ** \n`)
+    console.log(`\n *** server running on http://localhost:${PORT} ** \n`)
 })
--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 const express = require("express")
 const morgan = require("morgan")
 const helmet = require("helmet")
-const cors = require("cors")
+
 
 
 //import routers here
-const projectRouter = require("./router/projectRouter")
-const taskRouter = require("./router/taskRouter")
-const resourceRouter = require("./router/resourceRouter")
+// const projectRouter = require("./router/projectRouter")
+// const taskRouter = require("./router/taskRouter")
+// const resourceRouter = require("./router/resourceRouter")
 
 //set express to server
 const server = express()
@@ -16,11 +16,16 @@ const server = express()
 //use middlewear & routers here
 server.use(helmet())
 server.use(morgan("dev"))
-server.use(cors())
 server.use(express.json())
-server.use("/api/projects", projectRouter)
-server.use("/api/tasks", taskRouter)
-server.use("/api/resources", resourceRouter)
+// server.use("/api/projects", projectRouter)
+// server.use("/api/tasks", taskRouter)
+// server.use("/api/resources", resourceRouter)
+
+
+//test GET REQUEST
+server.get("/test", (req,res) => {
+    res.status(200).json({successMessage: "Basic server was a success"})
+})
 
 
 //export server here
